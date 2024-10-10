2a80890f3788a56a553a63e0759cfc2d5ab6cc62
Elena Dementeva <dementevayelena@gmail.com>
2024-10-10 23:49:11 +0400

    Delete dublicate script and add docstrings to the functions

--- scripts/dna_rna_tools.py
+++ scripts/dna_rna_tools.py
@@ -1,15 +1,44 @@
 def transcribe(sequence: str) -> str:
-    # РНК не должна быть для транскрипции
+    """
+    Converts a DNA sequence to RNA by replacing 'T' with 'U'.
+    
+    Args:
+        sequence (str): DNA sequence.
+    
+    Returns:
+        str: RNA sequence.
+    
+    Raises:
+        ValueError: If the sequence contains RNA ('U' or 'u').
+    """
     if 'U' in sequence or 'u' in sequence:
         raise ValueError("RNA detected. Only DNA can be transcribed.")
     return sequence.replace('T', 'U').replace('t', 'u')
 
 
 def reverse(sequence: str) -> str:
+    """
+    Reverse the sequence.
+    
+    Args:
+        sequence (str): Input sequence.
+    
+    Returns:
+        str: Reversed sequence.
+    """
     return sequence[::-1]
 
 
 def complement(sequence: str) -> str:
+    """
+    Returns the complement of a DNA/RNA sequence.
+    
+    Args:
+        sequence (str): Input sequence.
+    
+    Returns:
+        str: Complement sequence.
+    """
     complement_dict = {
         'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C',
         'a': 't', 't': 'a', 'c': 'g', 'g': 'c',
@@ -19,13 +48,30 @@ def complement(sequence: str) -> str:
 
 
 def reverse_complement(sequence: str) -> str:
+    """
+    Returns the reverse complement of a sequence.
+    
+    Args:
+        sequence (str): Input sequence.
+    
+    Returns:
+        str: Reverse complement sequence.
+    """
     return reverse(complement(sequence))
 
 
 def validate_sequence(sequence: str) -> None:
+    """
+    Checks if a sequence is DNA or RNA.
+    
+    Args:
+        sequence (str): Sequence to validation.
+    
+    Raises:
+        ValueError: If the sequence is invalid or contains T and U.
+    """
     valid_dna = "ATCGatcg"
     valid_rna = "AUCGaucg"
-    # T и U не должны быть вместе
     if 'U' in sequence or 'u' in sequence:
         if 'T' in sequence or 't' in sequence:
             raise ValueError("contains both T and U")
@@ -34,26 +80,3 @@ def validate_sequence(sequence: str) -> None:
     else:
         if any(nucleotide not in valid_dna for nucleotide in sequence):
             raise ValueError("Invalid for DNA")
-
-
-def run_dna_rna_tools(*args: str):
-    # Распаковка аргументов
-    *sequences, procedure = args
-
-    results = []
-
-    for seq in sequences:
-        validate_sequence(seq)
-        if procedure == "transcribe":
-            results.append(transcribe(seq))
-        elif procedure == "reverse":
-            results.append(reverse(seq))
-        elif procedure == "complement":
-            results.append(complement(seq))
-        elif procedure == "reverse_complement":
-            results.append(reverse_complement(seq))
-        else:
-            raise ValueError("Procedure is unknown")
-
-    # если одна последовательность -строкf, несколько - список
-    return results[0] if len(results) == 1 else results
